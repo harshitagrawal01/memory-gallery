9a5719954530f65fce68b0d168fbfcccce72178c
Harshit Agrawal <harshit0114ag@gmail.com>
2026-05-10 22:43:58 +0530

    updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    'ngrok-skip-browser-warning': 'true'
-  }
 });
 
 axiosInstance.interceptors.request.use((config) => {
